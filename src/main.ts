142355e333da1696722eddf895b8e599cc97b476
Karr4 but from another universe <XGon20987@gmail.com>
2025-08-04 17:59:55 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { MessagesModule } from './messages/messages.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(ComputerModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
